b5a82da851b316936891d62301cbe81e9e937a18
André Davies <andredavies.dev@gmail.com>
2023-05-08 14:57:11 -0300

    Refactoring code

--- src/test/java/com/davcode/minhasfinancas/model/repository/UserRepositoryTest.java
+++ src/test/java/com/davcode/minhasfinancas/model/repository/UserRepositoryTest.java
@@ -5,21 +5,27 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
     @Autowired
     UserRepository repository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
     @Test
     public void shouldVerifyIfEmailAlreadyExists(){
         User user = User.builder().name("User").email("user@email.com").build();
-        repository.save(user);
+        entityManager.persist(user);
 
         boolean result = repository.existsByEmail("user@email.com");
 
@@ -28,7 +34,6 @@ public class UserRepositoryTest {
 
     @Test
     public void shouldReturnFalseWhenDontHaveRegisteredUserWithEmail(){
-        repository.deleteAll();
         boolean result = repository.existsByEmail("user@email.com");
 
         Assertions.assertThat(result).isFalse();
