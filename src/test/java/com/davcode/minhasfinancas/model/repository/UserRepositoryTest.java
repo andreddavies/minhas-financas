9c9d8c694eafea4cea4aabcc95d13ed0f98ef8c3
André Davies <andredavies.dev@gmail.com>
2023-05-09 12:59:53 -0300

    Adding tests to user repository and updating

--- src/test/java/com/davcode/minhasfinancas/model/repository/UserRepositoryTest.java
+++ src/test/java/com/davcode/minhasfinancas/model/repository/UserRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJpaTest
@@ -24,18 +26,52 @@ public class UserRepositoryTest {
 
     @Test
     public void shouldVerifyIfEmailAlreadyExists(){
-        User user = User.builder().name("User").email("user@email.com").build();
+        User user = createUser();
         entityManager.persist(user);
 
-        boolean result = repository.existsByEmail("user@email.com");
+        boolean result = repository.existsByEmail("andre@email.com");
 
         Assertions.assertThat(result).isTrue();
     }
 
     @Test
     public void shouldReturnFalseWhenDontHaveRegisteredUserWithEmail(){
-        boolean result = repository.existsByEmail("user@email.com");
+        boolean result = repository.existsByEmail("andre@email.com");
 
         Assertions.assertThat(result).isFalse();
     }
+
+    @Test
+    public void shouldPersistUserInDatabase(){
+        User user = createUser();
+
+        User savedUser = repository.save(user);
+
+        Assertions.assertThat(savedUser.getId()).isNotNull();
+    }
+
+    @Test
+    public void shouldFindUserByEmail(){
+        User user = createUser();
+        entityManager.persist(user);
+
+        Optional<User> result = repository.findByEmail("andre@email.com");
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    public void shouldReturnEmptyIfEmailDoesntExitsInDatabase(){
+        Optional<User> result = repository.findByEmail("andre@email.com");
+        Assertions.assertThat(result.isPresent()).isFalse();
+    }
+
+    public static User createUser(){
+        return User
+                .builder()
+                .name("André")
+                .email("andre@email.com")
+                .password("password")
+                .build();
+    }
+
 }
